75260741e282d19eae70c0587dc0520df241754d
Luis Costa <129795162+Luis-000@users.noreply.github.com>
2026-05-09 19:09:41 +0100

    Add files via upload

---
+++ RectangleAreaCalculator.py
@@ -0,0 +1,8 @@
+#Calculate the area of a rectangle (A = W*l)
+
+lenght = float(input("Enter the lenght: "))
+widht = float(input("Enter the widht: "))
+
+area = lenght * widht
+
+print(f"The area of your rectangle is {area}cm²")
